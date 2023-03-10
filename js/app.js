59cdec62930d5552b0510abcda45b8571158d766
LilitVardanyan1992 <lvardanyan890@gmail.com>
2023-03-09 23:51:38 -0800

    changes

---
+++ js/app.js
@@ -0,0 +1,11 @@
+"css.lint.emptyRules": {
+    "type": "string",
+    "scope": "resource",
+    "enum": [
+      "ignore",
+      "warning",
+      "error"
+    ],
+    "default": "warning",
+    "description": "%css.lint.emptyRules.desc%"
+  },
